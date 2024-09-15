e85b7dc81a541f2656a8ba1d71f069b12cdb625d
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-09-15 11:16:09 +0800

    Fin Testimonial Sec

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -6,6 +6,7 @@ import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
 import { SectionHeader } from "@/components/SectionHeader";
 import grainImage from "@/assets/images/grain.jpg";
 import Image from "next/image";
+import { Card } from "@/components/Card";
 
 const testimonials = [
   {
@@ -42,19 +43,27 @@ const testimonials = [
 
 export const TestimonialsSection = () => {
   return (
-  <div className="py-16">
+  <div className="py-16 lg:py-24">
     <div className="container">
     <SectionHeader eyebrow="Happy Clients" title="What Clients Say about Me" description="Don't just take my word for it. See what my client have to say about my work"/>
-    <div>
+    <div className="mt-16 lg:mt-24 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+      <div className="flex gap-8 flex-none">
       {testimonials.map(testimonials => (
-        <div key={testimonials.name} className="bg-gray-800 rounded-3xl p-6 ">
-          <Image src={testimonials.avatar } alt={testimonials.name}/>
-          <div>{testimonials.name}</div>
-          <div>{testimonials.position}</div>
-          <p>{testimonials.text}</p>
-        </div>
+        <Card key={testimonials.name} className="max-w-xs md:max-w-md md:p-8">
+            <div className="flex gap-4 items-center">
+              <div className="size-14 bg-gray-700 inline-flex items-center justify-center rounded-full flex-shrink-0">
+                <Image src={testimonials.avatar } alt={testimonials.name} className="max-h-full"/>
+                </div>
+                <div>
+                  <div className="font-semibold ">{testimonials.name}</div>
+                  <div className="text-sm text-white/40">{testimonials.position}</div>
+                </div>
+            </div>
+            <p className="mt-4 md:mt-6 text-sm md:text-base">{testimonials.text}</p>
+        </Card>
         
       ))}
+      </div>
     </div>
     </div>
   </div>
